5b4284e01fa0d846f9869b8066dc2c80a687bea5
Patrick <rosevalley@gmail.com>
2023-05-27 21:39:02 +0200

    Initial Commit

---
+++ guessTheFlagProject/guessTheFlagProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessTheFlagProjectApp.swift
+//  guessTheFlagProject
+//
+//  Created by User on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct guessTheFlagProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
