1058de68afc6956763d919a333710d790f6b0321
PatrickLubbers <134800717+PatrickLubbers@users.noreply.github.com>
2023-05-31 19:38:51 +0200

    Update ContentView.swift
    added content view to project

--- ContentView.swift
+++ ContentView.swift
@@ -7,10 +7,6 @@
 
 //SwiftUI has a dedicated Image type for handling pictures in your apps, and there are three main ways you will create them:
 
-//Image("pencil") will load an image called “Pencil” that you have added to your project.
-//Image(decorative: "pencil") will load the same image, but won’t read it out for users who have enabled the screen reader. This is useful for images that don’t convey additional important information.
-//Image(systemName: "pencil") will load the pencil icon that is built into iOS. This uses Apple’s SF Symbols icon collection, and you can search for icons you like – download Apple’s free SF Symbols app from the web to see the full set.
-
 import SwiftUI
 
 struct ContentView: View {
